deb5d0ae23e3103d0a574462a9c168c57b044b2c
choudhurysr <sanglapchoudhury@gmail.com>
2018-08-31 15:18:50 -0400

    First commit

---
+++ src/main/java/learn/RestTestController.java
@@ -0,0 +1,20 @@
+package learn;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestTestController {
+	private List<String> dataList = new ArrayList<String>(Arrays.asList("Spring", "Rest", "SRC"));
+	
+    @RequestMapping("/getData")
+    public Data getData(@RequestParam(value="id", defaultValue="0") Integer id) {
+        return new Data(id, dataList.get(id));
+    }
+}
